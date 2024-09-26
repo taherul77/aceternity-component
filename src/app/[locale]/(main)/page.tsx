9231d1e739f82429e7404be96a2bf4dc41eaf86c
taherul <sktaherul@atilimited.net>
2024-09-26 12:02:43 +0530

    update project

--- src/app/[locale]/(main)/page.tsx
+++ src/app/[locale]/(main)/page.tsx
@@ -1,5 +1,9 @@
 import { AppleCardsCarouse } from "@/components/ui/AppleCardsCarouse/AppleCardsCarouse";
 import BackgroundBeamsWithCollisionComp from "@/components/ui/BackgroundBeamsWithCollisionComp/BackgroundBeamsWithCollision";
+import BackgroundLinesComp from "@/components/ui/BackgroundLines/BackgroundLinesComp";
+import CardSpotlightComp from "@/components/ui/CardSpotlightComp/CardSpotlightComp";
+import CompareAtuoplay from "@/components/ui/CompareAtuoplay/CompareAtuoplay";
+import CompareComp from "@/components/ui/CompareComp/CompareComp";
 import { MacbookScroll } from "@/components/ui/macbook-scroll";
 import ThreeDCard from "@/components/ui/ThreeDCard/ThreeDCard";
 
@@ -10,8 +14,16 @@ const page = () => {
       <div className="my-10">
         <ThreeDCard></ThreeDCard>
       </div>
-	  <AppleCardsCarouse/>
-	  <BackgroundBeamsWithCollisionComp/>
+      <AppleCardsCarouse />
+      <BackgroundBeamsWithCollisionComp />
+      <BackgroundLinesComp />
+      <div className="flex justify-center w-full mx-auto gap-10">
+        <CardSpotlightComp></CardSpotlightComp>
+        <CompareComp></CompareComp>
+      </div>
+      <div className="flex justify-center items-center py-5">
+        <CompareAtuoplay></CompareAtuoplay>
+      </div>
     </div>
   );
 };
