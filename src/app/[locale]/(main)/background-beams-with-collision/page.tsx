43f70b32517bc8341c3eaf48232ac79ad5ec4b92
taherul77 <taherul2317@gmail.com>
2024-10-05 22:34:11 +0600

    add BackgroundBeamsWithCollisionComp page

---
+++ src/app/[locale]/(main)/background-beams-with-collision/page.tsx
@@ -0,0 +1,12 @@
+import BackgroundBeamsWithCollisionComp from '@/components/ui/BackgroundBeamsWithCollisionComp/BackgroundBeamsWithCollision'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <BackgroundBeamsWithCollisionComp/>
+    </div>
+  )
+}
+
+export default page
--- src/components/ui/DashboardComp/SidebarComp/SidebarComp.tsx
+++ src/components/ui/DashboardComp/SidebarComp/SidebarComp.tsx
@@ -42,6 +42,13 @@ const SidebarComp = ({ Children }:SidebarCompProps) => {
       ),
     },
     {
+      label: "Background Beams with Collision",
+      href: "/en/background-beams-with-collision",
+      icon: (
+        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
       label: "Settings",
       href: "#",
       icon: (
